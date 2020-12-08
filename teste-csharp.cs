626ef7b7c04622fb5f7581247575dfbea3082cdd
Gabriela Lopes <gabriela.a_lopes@hotmail.com>
2020-12-07 22:03:04 -0300

    Create teste-csharp.cs

---
+++ teste-csharp.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace TesteCSharp9._0
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        public void notificar (string nome)
+        {
+            if (nome is null)
+            {
+                //Instrucao ...
+            }
+        }
+    }
+}
